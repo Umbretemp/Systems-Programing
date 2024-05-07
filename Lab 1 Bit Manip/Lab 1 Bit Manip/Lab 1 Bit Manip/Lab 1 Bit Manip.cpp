595b5856f2acb0ab06031174f97c40196714bc2b
Jacob Cotham <JACotham@student.fullsail.edu>
2024-05-07 18:01:35 -0400

    Input

--- Lab 1 Bit Manip/Lab 1 Bit Manip/Lab 1 Bit Manip/Lab 1 Bit Manip.cpp
+++ Lab 1 Bit Manip/Lab 1 Bit Manip/Lab 1 Bit Manip/Lab 1 Bit Manip.cpp
@@ -8,38 +8,13 @@ void Print(const unsigned int _x); //decleration
 
 int main()
 {
+    Print(bitField);
 
-
-    // NOT (~)
-    //int z = 167;
-    //Print(z);
-    //z = ~z;
-    //Print(z);
-    //int a = UINT_MAX;
-    //Print(a);
-    //a = ~a;
-    //Print(a);
-
-    // AND (&)
-    //int f = 187;
-    //Print(f);
-    //int g = 243;
-    //Print(g);
-    //int h = f & g;
-    //Print(h);
-
-    // OR (|)
-    //int f = 187;
-    //Print(f);
-    //int g = 243;
-    //Print(g);
-    //int h = f | g;
-    //Print(h);
 }
 
 void Print(const unsigned int _x) //definition
 {
-    std::cout << "DEC: " << _x << "\t";
+    std::cout << "BITFIELD: " << _x << "\t";
     std::cout << std::hex << "HEX: " << _x << std::dec << "\t";
     std::cout << "BIN: " << std::bitset<32>(_x) << "\n";
 }
@@ -49,6 +24,22 @@ void Turnon(int& bit)
     // bitfield 0001 0011 - current value
     // user passed in 3 as bit to turn on
     // figure out what operator to use and what value
+    bitField = bitField | bit;
     // --------------------
     // bitfield 0001 1011
+}
+
+void TurnOff(int& bit)
+{
+    // bitfield 0001 0011 - current value
+    // user passed in X as bit to turn off
+    // figure out what operator to use and what value
+    bitField = bitField & ~bit;
+    // --------------------
+    // bitfield 
+}
+
+void Toggle(int& bit)
+{
+    bitField = bitField ^ bit;
 }
