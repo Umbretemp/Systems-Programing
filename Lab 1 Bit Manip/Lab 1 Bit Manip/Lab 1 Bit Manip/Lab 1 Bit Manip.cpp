32d41ef84677211c670c574f39ab56e5eb8ce7c4
Jacob Cotham <JACotham@student.fullsail.edu>
2024-05-08 16:36:29 -0400

    Almsot done

--- Lab 1 Bit Manip/Lab 1 Bit Manip/Lab 1 Bit Manip/Lab 1 Bit Manip.cpp
+++ Lab 1 Bit Manip/Lab 1 Bit Manip/Lab 1 Bit Manip/Lab 1 Bit Manip.cpp
@@ -4,10 +4,13 @@
 #include "../../../Misc/Console/Console.h"
 
 
-unsigned int bitField = 17; //Global variable
+unsigned int bitField = 777; //Global variable
 void Turnon(int bit); //decleration
 void TurnOff(int bit);
 void Toggle(int bit);
+void Negate();
+void LeftShift();
+void RightShift();
 
 void Print(const unsigned int _x); //decleration
 
@@ -15,12 +18,11 @@ int main()
 {
     int menuselection = 0;
     std::vector<std::string> menuOptions{ "1. Turn On", "2. Turn Off", "3. Toggle", "4. Negate", "5. Left Shift", "6. Right Shift", "7. Exit"};
-    
+    Print(bitField);
 
     do
     {
 
-        Print(bitField);
         menuselection = Input::GetMenuSelection(menuOptions);
         Console::Clear();
 
@@ -43,17 +45,17 @@ int main()
         }
         case 4: // Negate
         {
-
+            Negate();
             break;
         }
         case 5: // Left Shift
         {
-
+            LeftShift();
             break;
         }
         case 6: // Right Shift
         {
-
+            RightShift();
             break;
         }
         default:
@@ -76,10 +78,10 @@ void Turnon(int bit)
     // bitfield 0001 0011 - current value
     // user passed in 3 as bit to turn on
     // figure out what operator to use and what value
-    unsigned int on = bitField | 1<<bit;
+    bitField = bitField | 1 << bit;
     // --------------------
     // bitfield 0001 1011
-    Print(on);
+    Print(bitField);
 }
 
 void TurnOff(int bit)
@@ -87,14 +89,32 @@ void TurnOff(int bit)
     // bitfield 0001 0011 - current value
     // user passed in X as bit to turn off
     // figure out what operator to use and what value
-    unsigned int off = bitField & ~0<< bit;
+    bitField = bitField & ~(1 << bit);
     // --------------------
     // bitfield 
-    Print(off);
+    Print(bitField);
 }
 
 void Toggle(int bit)
 {
-    unsigned int Toggle = bitField ^ 1<<bit;
-    Print(Toggle);
+    bitField = bitField ^ 1<<bit;
+    Print(bitField);
+}
+
+void Negate()
+{
+    bitField = ~bitField;
+    Print(bitField);
+}
+
+void LeftShift()
+{
+    bitField = bitField << 1;
+    Print(bitField);
+}
+
+void RightShift()
+{
+    bitField = bitField >> 1;
+    Print(bitField);
 }
