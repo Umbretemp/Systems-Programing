345bda044a9d5c74a338575d66f814800af64863
Jacob Cotham <JACotham@student.fullsail.edu>
2024-05-14 16:03:41 -0400

    Credit feedback

--- Lab 4 - Inheritance/Lab 4 - Inheritance/Lab 4 - Inheritance/CreditAccount.cpp
+++ Lab 4 - Inheritance/Lab 4 - Inheritance/Lab 4 - Inheritance/CreditAccount.cpp
@@ -5,7 +5,9 @@ void CreditAccount::Withdraw(float amount)
 {
 	ammountSpent_ += amount;
 	if (ammountSpent_ > 40)
+	{
 		amount += 5000;
-
+		std::cout << "$5000.00 fee added to each transaction after you exceed your $40.00 credit limit. \n";
+	}
 	BaseAccount::Withdraw(amount);
 }
