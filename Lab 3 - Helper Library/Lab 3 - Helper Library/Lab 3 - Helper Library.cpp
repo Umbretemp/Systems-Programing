4dbfbfd1450ac379e6b6e8411aba87654b23befc
Jacob Cotham <JACotham@student.fullsail.edu>
2024-05-14 12:31:04 -0400

    lab 3

--- Lab 3 - Helper Library/Lab 3 - Helper Library/Lab 3 - Helper Library.cpp
+++ Lab 3 - Helper Library/Lab 3 - Helper Library/Lab 3 - Helper Library.cpp
@@ -11,4 +11,5 @@ int main()
 
     std::cout << "You entered " << std::to_string(GetValidatedInt("Please enter a number: ", 500, INT_MAX)) << "\n";
 
+    std::cout << "You entered " << std::to_string(GetValidatedInt("Please enter a number: ", INT_MIN, 500)) << "\n";
 }
