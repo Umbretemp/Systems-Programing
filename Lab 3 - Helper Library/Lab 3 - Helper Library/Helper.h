3298c17787bf2905435857ccff82e9166b3c957a
Jacob Cotham <JACotham@student.fullsail.edu>
2024-05-14 12:37:59 -0400

    TO DO: test letters and why returned 0

--- Lab 3 - Helper Library/Lab 3 - Helper Library/Helper.h
+++ Lab 3 - Helper Library/Lab 3 - Helper Library/Helper.h
@@ -14,31 +14,24 @@ namespace Helper
 		if (nMaximumRange == 0 && nMinimumRange == 0) // Prefer (min == max) instead of both = 0
 			return 0;
 
-		std::cin >> Input;
-		if (std::cin.fail()) // entered non integer
-		{
-			std::cin.clear(); // clears any errors in cin
-			std::cin.ignore(INT_MAX, '\n'); // flush INT_MAX chars until it sees '\n'
-		}
-		else
-			std::cin.clear();// the buffer still needs to be cleared even if this step was successful
+		
 
 		do
 		{
+			std::cin >> Input;
+			if (std::cin.fail()) // entered non integer
+			{
+				std::cin.clear(); // clears any errors in cin
+				std::cin.ignore(INT_MAX, '\n'); // flush INT_MAX chars until it sees '\n'
+			}
+			else
+				std::cin.clear();// the buffer still needs to be cleared even if this step was successful
+
 			if (Input <= nMaximumRange && Input >= nMinimumRange)
 				inRange = true;
-			else
-			{
+			else			
 				std::cout << "Please enter a value between " << nMinimumRange << " and " << nMaximumRange << "\n";
-				std::cin >> Input;
-				if (std::cin.fail()) // entered non integer
-				{
-					std::cin.clear(); // clears any errors in cin
-					std::cin.ignore(INT_MAX, '\n'); // flush INT_MAX chars until it sees '\n'
-				}
-				else
-					std::cin.clear();// the buffer still needs to be cleared even if this step was successful
-			}
+
 		}	while (!inRange);
 
 		return Input;
