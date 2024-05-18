13f7af11fe4c933e0b0c5a5206f102864f1936d2
Jacob Cotham <JACotham@student.fullsail.edu>
2024-05-18 13:57:17 -0400

    Menu barebones

--- Lab 4 - Inheritance/Lab 4 - Inheritance/Lab 4 - Inheritance/Lab 4 - Inheritance.cpp
+++ Lab 4 - Inheritance/Lab 4 - Inheritance/Lab 4 - Inheritance/Lab 4 - Inheritance.cpp
@@ -16,6 +16,106 @@ int main()
 	CheckingAccount* CheckingPTR = new CheckingAccount(5000);
 	SavingsAccount* SavingsPTR = new SavingsAccount(2000);
 	CreditAccount* CreditPTR = new CreditAccount(50000);
+	int menuSelection = 0;
+	std::vector<std::string> bankMenu{ "1. Checking Account", "2. Savings Account","3. Credit Account", "4. Exit" };
+	int accountSelection = 0;
+	std::vector<std::string> accountMenu{ "1. Deposit", "2. Withdrawl", "3. Check Balance", "4. Exit" };
+
+	do
+	{
+		Console::Clear();
+		menuSelection = Input::GetMenuSelection(bankMenu);
+		Console::Clear();
+
+		
+
+		switch (menuSelection)
+		{
+		case 1: // Checking
+		{
+			Console::Clear();
+			menuSelection = Input::GetMenuSelection(accountMenu);
+			Console::Clear();
+
+			do
+			{
+				switch (accountSelection)
+				{
+				case 1: // Deposit
+				{
+				}
+				case 2: // Withdrawl
+				{
+				}
+				case 3: // Balance
+				{
+				}
+				default:
+					break;
+				}
+			} while (accountSelection != accountMenu.size());
+
+			break;
+		}
+		case 2: // Savings
+		{
+			Console::Clear();
+			menuSelection = Input::GetMenuSelection(accountMenu);
+			Console::Clear();
+
+			do
+			{
+				switch (accountSelection)
+				{
+				case 1: // Deposit
+				{
+				}
+				case 2: // Withdrawl
+				{
+				}
+				case 3: // Balance
+				{
+				}
+				default:
+					break;
+				}
+			} while (accountSelection != accountMenu.size());
+
+			break;
+		}
+		case 3: // Credit
+		{
+			Console::Clear();
+			menuSelection = Input::GetMenuSelection(accountMenu);
+			Console::Clear();
+
+			do
+			{
+				switch (accountSelection)
+				{
+				case 1: // Deposit
+				{
+				}
+				case 2: // Withdrawl
+				{
+				}
+				case 3: // Balance
+				{
+				}
+				default:
+					break;
+				}
+			} while (accountSelection != accountMenu.size());
+
+			break;
+		}
+		default:
+			break;
+		}
+
+		Input::PressEnter(true);
+
+	} while (menuSelection != bankMenu.size());
 
 	//CheckingAccount Test(5000, 0);
 	//SavingsAccount TESTER(5000, 0);
