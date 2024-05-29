40560b94abc9d69472d9816ccb4128bc97f6ce5b
Jacob Cotham <JACotham@student.fullsail.edu>
2024-05-29 18:33:47 -0400

    name commits

--- Lab 8 - Rule of 3/Lab 8 - Rule of 3/Base.cpp
+++ Lab 8 - Rule of 3/Lab 8 - Rule of 3/Base.cpp
@@ -34,8 +34,8 @@ void Base::setName(const char* name)
 	}
 	// find the length of the array passed in
 	int length = strlen(name) + 1; // add one for the null terminator
-	// new an array of that size for model to point to
+	// new an array of that size for _name to point to
 	_name = new char[length];
-	// copy the chars from _model to model
-	strcpy_s(_name, length, name); // copies chars from _model to model until sees a null terminator
+	// copy the chars from name to _name
+	strcpy_s(_name, length, name); // copies chars from name to _name until sees a null terminator
 }
