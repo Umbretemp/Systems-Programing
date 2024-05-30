27f72b52076f062c116e9a6c24f1253c5ffca0b9
Jacob Cotham <JACotham@student.fullsail.edu>
2024-05-30 14:17:01 -0400

    Char fixed

--- Lab 8 - Rule of 3/Lab 8 - Rule of 3/Lab 8 - Rule of 3.cpp
+++ Lab 8 - Rule of 3/Lab 8 - Rule of 3/Lab 8 - Rule of 3.cpp
@@ -59,11 +59,12 @@ void AddRecord(std::vector<Base*>& vect)
 		if (choice == 1)
 		{
 			Employee* newEmployee = new Employee();
-			char* name[32];
+			char name[32];
 			int Salary = 0;
 			std::cout << "What is the Employee's Name?\n";
-			std::cin >> name[32];
-			newEmployee->setName(name[32]);
+			std::cin >> name;
+			char* nptr = name;
+			newEmployee->setName(nptr);
 			std::cout << "What is the Employee's Salary?\n";
 			std::cin >> Salary;
 			newEmployee->setSalary(Salary);
@@ -75,15 +76,16 @@ void AddRecord(std::vector<Base*>& vect)
 		else if (choice == 2)
 		{
 			Student* newStudent = new Student();
-			char* name[32];
+			char name[32];
 			float GPA = 0.00;
-			std::cout << "What is the Employee's Name?\n";
-			std::cin >> name[32];
-			newStudent->setName(name[32]);
-			std::cout << "What is the Employee's Salary?\n";
+			std::cout << "What is the Student's Name?\n";
+			std::cin >> name;
+			char* nptr = name;
+			newStudent->setName(nptr);
+			std::cout << "What is the Student's GPA?\n";
 			std::cin >> GPA;
 			newStudent->setGPA(GPA);
-			std::cout << "New Employee added\t";
+			std::cout << "New Student added\t";
 			newStudent->DisplayRecord();
 			vect.push_back(newStudent);
 			delete newStudent;
@@ -105,17 +107,18 @@ void DuplicateRecord(std::vector<Base*>& vect)
 {
 	int index = -1;
 	bool valid = false;
-	do
-	{
-		std::cout << "Enter the index of the record you would like to duplicate: ";
-		std::cin >> index;
+	index = Helper::GetValidatedInt("Enter the index of the record you would like to duplicate: ", 0, vect.size());
 
-		if (index < 0 || index >= vect.size())
-		{
-			std::cout << "Invalid index.\n";
-			valid = true;
-		}
-	} while (true);
+	//do
+	//{
+	//	std::cout << "Enter the index of the record you would like to duplicate: ";
+	//	std::cin >> index;
+	//	if (index < 0 || index >= vect.size())
+	//	{
+	//		std::cout << "Invalid index.\n";
+	//		valid = true;
+	//	}
+	//} while (true);
 	
 	vect.push_back(vect[index]);
 }
