17449bf0b5a5c7e9c2c0d70b54305450a6b370e3
Jacob Cotham <JACotham@student.fullsail.edu>
2024-05-31 20:54:40 -0400

    Triangleheap class rule of 3 definitions created

--- Lab 9 - Pointers Part Two/Lab 9 - Pointers Part Two/TriangleHeap.cpp
+++ Lab 9 - Pointers Part Two/Lab 9 - Pointers Part Two/TriangleHeap.cpp
@@ -1,5 +1,35 @@
 #include "TriangleHeap.h"
 
+TriangleHeap::TriangleHeap() : mBase(nullptr), mHeight(nullptr)
+{
+    setBase(0.0f);
+    setHeight(0.0f);
+}
+
+TriangleHeap& TriangleHeap::operator=(const TriangleHeap& _other)
+{
+    // TODO: insert return statement here
+    if (this != &_other) // if they have the same memory address - same object don't need to do anything
+	{
+		*mBase = *(_other.mBase); 
+		*mHeight = *(_other.mHeight); 
+	}
+
+	return *this; // this is a pointer to the invoking object
+}
+
+TriangleHeap::TriangleHeap(const TriangleHeap& _other)
+{
+    // need to fix any shallow copies
+    *this = _other; // just call the assignment operator as it does out deep copies for us
+}
+
+TriangleHeap::~TriangleHeap()
+{
+    delete mBase;
+    delete mHeight;
+}
+
 float TriangleHeap::GetArea()
 {
     //area is base * height / 2
