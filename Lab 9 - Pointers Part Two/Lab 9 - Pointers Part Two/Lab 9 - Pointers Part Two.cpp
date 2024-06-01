cb67682b47e707f044f21a9d9656f94c2233aa0e
Jacob Cotham <JACotham@student.fullsail.edu>
2024-05-31 20:10:57 -0400

    functions()

--- Lab 9 - Pointers Part Two/Lab 9 - Pointers Part Two/Lab 9 - Pointers Part Two.cpp
+++ Lab 9 - Pointers Part Two/Lab 9 - Pointers Part Two/Lab 9 - Pointers Part Two.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 
+int AddNumbersReturnInt(int* a, int* b);
+void AddNumbersReturnVoid(int a, int b, int* sum);
+void SwapValues(int* a, int* b);
+
 int main()
 {
     std::cout << "Hello World!\n";
+}
+
+int AddNumbersReturnInt(int* a, int* b)
+{
+    int result;
+
+    result = *a + *b;
+
+    return result;
+}
+
+void AddNumbersReturnVoid(int a, int b, int* sum)
+{
+    int result;
+
+    result = a + b;
+
+    *sum = result;
+}
+
+void SwapValues(int* a, int* b)
+{
+    int tempA = *a;
+    int tempB = *b;
+    *a = tempB;
+    *b = tempA;
 }
