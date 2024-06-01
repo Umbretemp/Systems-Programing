65248cbedebb27efbc12ff0854424801ca7a25c3
Jacob Cotham <JACotham@student.fullsail.edu>
2024-05-31 21:15:12 -0400

    TriangleStack main, complete

--- Lab 9 - Pointers Part Two/Lab 9 - Pointers Part Two/Lab 9 - Pointers Part Two.cpp
+++ Lab 9 - Pointers Part Two/Lab 9 - Pointers Part Two/Lab 9 - Pointers Part Two.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include "TriangleHeap.h"
+#include "TriangleStack.h"
+#include <vector>
 
 int AddNumbersReturnInt(int* a, int* b);
 void AddNumbersReturnVoid(int a, int b, int* sum);
@@ -6,7 +9,22 @@ void SwapValues(int* a, int* b);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    TriangleStack Tri1;
+    Tri1.setBase(3.6);
+    Tri1.setHeight(7.1);
+    TriangleStack Tri2;
+    Tri2.setBase(1.6);
+    Tri2.setHeight(5.9);
+    std::vector<TriangleStack> triVect;
+    triVect.push_back(Tri1);
+    triVect.push_back(Tri2);
+    for (auto i = 0; i < triVect.size(); i++)
+    {
+        std::cout << "TriangleStack#" << i << " " << triVect[i].GetArea() << "\n";
+    }
+
+
+
 }
 
 int AddNumbersReturnInt(int* a, int* b)
