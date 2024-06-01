97cf9c3fa69aa31ad4e802731ec6f6521a5215db
Jacob Cotham <JACotham@student.fullsail.edu>
2024-05-31 20:48:43 -0400

    Rule of 3 declarations for triangleheap class

--- Lab 9 - Pointers Part Two/Lab 9 - Pointers Part Two/TriangleHeap.h
+++ Lab 9 - Pointers Part Two/Lab 9 - Pointers Part Two/TriangleHeap.h
@@ -6,7 +6,12 @@ private:
 	float* mHeight = (float*)nullptr;
 
 public:
-
+	TriangleHeap(); // default
+	// rule of 3
+	TriangleHeap& operator=(const TriangleHeap& _other); // assignment operator
+	TriangleHeap(const TriangleHeap& _other); // copy constructor
+	~TriangleHeap(); // destructor - gets called when object gets destroyed
+	
 	void setBase(float base) { *mBase = base; }
 	void setHeight(float height) { *mHeight = height; }
 
